05fb081aa4fd8f6d368338ff3bb0c85f49a7bbdc
yyc <493408482@qq.com>
2020-09-04 17:08:02 +0800

    refacotr:update

--- Python/21-merge-two-sorted-lists.py
+++ Python/21-merge-two-sorted-lists.py
@@ -1,13 +1,29 @@
 # https://leetcode.com/problems/merge-two-sorted-lists/
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
+    def list_to_node(self, vals: list) -> ListNode:
+        res = ListNode(vals[0])
+        current_node = res
+        for i in range(1, len(vals)):
+            current_node.next = ListNode(vals[i])
+            current_node = current_node.next
+        return res
+
+    def node_to_list(self, node):
+        vals = list()
+        vals.append(node.val)
+        while (node.next!=None):
+            vals.append(node.next.val)
+            node = node.next
+        return vals
+
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         if(l1==None):
             return l2
@@ -16,17 +32,22 @@ class Solution:
         
         # list listnode's val and sort
         vals = list()
-        for l in (l1, l2):
-            vals.append(l.val)
-            while (l.next!=None):
-                vals.append(l.next.val)
-                l = l.next
+        vals = self.node_to_list(l1) + self.node_to_list(l2)
         vals.sort()
         
         # refctor listnode
-        res = ListNode(vals[0])
-        current_node = res
-        for i in range(1, len(vals)):
-            current_node.next = ListNode(vals[i])
-            current_node = current_node.next 
-        return res
+        return self.list_to_node(vals)
+
+
+if __name__ == '__main__':
+    import sys
+    aa = [1,2,5,8]
+    bb = [1,2,4,9]
+    if len(sys.argv) == 3:
+        aa = eval(sys.argv[1])
+        bb = eval(sys.argv[2])
+    s = Solution()
+    l1 = s.list_to_node(aa)
+    l2 = s.list_to_node(bb)
+    res = s.mergeTwoLists(l1, l2)
+    print(f'l1 is {aa}, l2 is {bb}, res is {s.node_to_list(res)}')
--- Python/9-palindrome-number.py
+++ Python/9-palindrome-number.py
@@ -1,4 +1,7 @@
 # https://leetcode.com/problems/palindrome-number/
+import copy
+import math
+
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
@@ -10,15 +13,24 @@ class Solution:
         foo = 1
         y = copy.deepcopy(x)
         while foo != 0:
-                foo = y//10
-                y =  y / 10
-                digits += 1
+            foo = y//10
+            y =  y / 10
+            digits += 1
         num_list = list()
         for i in range(digits, 0, -1):
-                divisor = int('1'+'0'*(i-1))
-                num_list.append(x//divisor)
-                x = x % divisor
+            divisor = int('1'+'0'*(i-1))
+            num_list.append(x//divisor)
+            x = x % divisor
         for j in range(int(math.ceil(digits/2))):
-                if num_list[j] != num_list[-(j+1)]:
-                        return False
+            if num_list[j] != num_list[-(j+1)]:
+                return False
         return True
+
+
+if __name__ == '__main__':
+    import sys
+    num = int(sys.argv[1]) if len(sys.argv)==2 else 123454321
+    if Solution().isPalindrome(num) is True:
+        print(f'{num} is palindrome')
+    else:
+       print(f'{num} is not palindrome')
